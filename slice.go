bf5886e73943a1103e21083cd34a6c207c507bfd
Ttangc <759249230@qq.com>
2018-12-26 09:24:41 +0800

    12.26

--- slice.go
+++ slice.go
@@ -2,44 +2,44 @@ package main
 
 import "fmt"
 
-func main(){
-    s:=make([]string,3)
-    fmt.Println("s:",s);
-    s[0]="a"
-    s[1]="b"
-    s[2]="c"
-    fmt.Println("set:" ,s)
-    fmt.Println("get:",s[2])
+func main() {
+	s := make([]string, 3)
+	fmt.Println("s:", s)
+	s[0] = "a"
+	s[1] = "b"
+	s[2] = "c"
+	fmt.Println("set:", s)
+	fmt.Println("get:", s[2])
 
-    fmt.Println("s len:",len(s))
+	fmt.Println("s len:", len(s))
 
-    s = append(s,"d")
-    s = append(s,"e","f")
-    fmt.Println("append after s:",s)
+	s = append(s, "d")
+	s = append(s, "e", "f")
+	fmt.Println("append after s:", s)
 
-    c:=make([]string,len(s))
-    copy(c,s)
-    fmt.Println("copy c =",c)
+	c := make([]string, len(s))
+	copy(c, s)
+	fmt.Println("copy c =", c)
 
-    l:=s[2:5]
-    fmt.Println("sl1:",l)
+	l := s[2:5]
+	fmt.Println("sl1:", l)
 
-    l =s[:5]
-    fmt.Println("sl2:",l)
-    l =s[2:]
-    fmt.Println("sl3:",l)
+	l = s[:5]
+	fmt.Println("sl2:", l)
+	l = s[2:]
+	fmt.Println("sl3:", l)
 
-    t := []string{"g","h","i"}
-    fmt.Println("t:",t)
+	t := []string{"g", "h", "i"}
+	fmt.Println("t:", t)
 
-    twoD := make([][]int,3)
-    for i:=0;i<3;i++{
-        innerLen:=i+1
-        twoD[i]=make([]int,innerLen)
-        for j:=0;j<innerLen;j++{
-            twoD[i][j]=i+j
-        }
-    }
+	twoD := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoD[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoD[i][j] = i + j
+		}
+	}
 
-    fmt.Println("twoD:",twoD)
+	fmt.Println("twoD:", twoD)
 }
--- switch.go
+++ switch.go
@@ -4,33 +4,32 @@ import "fmt"
 
 import "time"
 
-func main(){
-    i:=2
-    fmt.Print("wirte",i,"as")
-    switch i{
-    case 1:
-        fmt.Println("one")
-    case 2:
-        fmt.Println("two")
-    case 3:
-        fmt.Println("three")
-    }
+func main() {
+	i := 2
+	fmt.Print("wirte", i, "as")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
 
-    switch time.Now().Weekday(){
-    case time.Saturday,time.Sunday:
-        fmt.Println("is is weekend")
-    default:
-        fmt.Println("it is weekday")
-    }
-    //t:=time.Now()
-    a := 12
-    switch{
-    case a<13:
-        fmt.Println(a,"a < 13")
-        continue
-    case a < 15:
-        fmt.Println(a,"a < 15")
-    default:
-        fmt.Println("it's after noon")
-    }
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("is is weekend")
+	default:
+		fmt.Println("it is weekday")
+	}
+	//t:=time.Now()
+	a := 12
+	switch {
+	case a < 13:
+		fmt.Println(a, "a < 13")
+	case a < 15:
+		fmt.Println(a, "a < 15")
+	default:
+		fmt.Println("it's after noon")
+	}
 }
